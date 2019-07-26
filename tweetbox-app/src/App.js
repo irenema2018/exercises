d927668be0c717d5ad0c885bf4c6cf8c197ff258
Irene Ma <irene.ma@outlook.com>
2019-07-26 23:14:41 +1000

    Add react exercise

---
+++ tweetbox-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import TweetBox from './TweetBox'
+
+function App() {
+  return (
+    <div className="App">
+      <TweetBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ tweetbox-app/src/TweetBox.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './TweetBox.css'
+import ProgressBar from './ProgressBar'
+// import { directive } from '@babel/types';
+
+export default class TweetBox extends React.Component {
+    state = {
+        content: '',
+    }
+
+    handleChange = event => {
+        this.setState({
+          content: event.target.value
+        })
+        console.log(this.state.content)
+      }
+    
+       
+    render = () => {
+        const maxContentLength = 140
+        const {content} = this.state 
+        const isDisabled = content.length === 0 || content.length > maxContentLength
+        const spanClass = content.length > 110 ?  'warning' : '' 
+    
+        return (
+            <div style = {{ width: "155px" }}>
+                {ProgressBar(content.length / maxContentLength * 100)}
+                <textarea
+                    onChange = {this.handleChange}
+                    placeholder = "type something..."
+                    row = "7"
+                ></textarea>
+                <footer>
+                    <span className={spanClass}>{maxContentLength - content.length}</span>
+                    <button disabled={isDisabled}>tweet</button>
+                </footer>
+            </div>
+        )
+    
+    
+    }
+
+
+}
