2ba521b64f3636665f92395e11242575fc69b834
Irene Ma <irene.ma@outlook.com>
2019-08-09 14:58:21 +1000

    Refactor and apply use hook useState

--- 0528_react_bankaccount/src/BankAccount.js
+++ 0528_react_bankaccount/src/BankAccount.js
@@ -1,67 +1,91 @@
-import React from 'react';
-//              template
-export default class BankAccount extends React.Component {
+import React, { useState } from 'react';
+
+
+export default function BankAccount (props) {
+// export default class BankAccount extends React.Component {
   
-  constructor (props) {
-    super()
+  // constructor (props) {
+  //   super()
     
-    this.getType = this.getType.bind(this)
-    this.withdraw = this.withdraw.bind(this)
-    this.deposit = this.deposit.bind(this)
-    this.showBalance = this.showBalance.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+  //   this.getType = this.getType.bind(this)
+  //   this.withdraw = this.withdraw.bind(this)
+  //   this.deposit = this.deposit.bind(this)
+  //   this.showBalance = this.showBalance.bind(this)
+  //   this.handleChange = this.handleChange.bind(this)
 
-    this.state = {
-      type: props.type,
-      balance: 0,
-      transactionHistory: [],
-      amount: 0
-    }
+  //   this.state = {
+  //     type: props.type,
+  //     balance: 0,
+  //     transactionHistory: [],
+  //     amount: 0
+  //   }
+  // }
+
+  const [type, setType]                             = useState(props.type)
+  const [balance, setBalance]                       = useState(0)
+  const [transactionHistory, setTransactionHistory] = useState([])
+  const [amount, setAmount]                         = useState(0)
+
+
+  const getType = () => {
+    // return this.state.type
+    return type
   }
 
-  getType () {
-    return this.state.type
+  const withdraw = () => {
+    // this.setState({
+    //   balance: Number(this.state.balance) - Number(this.state.amount),
+    //   // [...arr, newElement]
+    //   transactionHistory: [...this.state.transactionHistory, 'Withdraw ( ' + this.state.amount + ' )' ]
+    // })  
+    return setBalance(Number(balance) - Number(amount)),
+           setTransactionHistory([...transactionHistory, 'Withdraw ( ' + amount + ' )' ])
+
   }
 
-  withdraw () {
-    this.setState({
-      balance: Number(this.state.balance) - Number(this.state.amount),
-      // [...arr, newElement]
-      transactionHistory: [...this.state.transactionHistory, 'Withdraw [' + this.state.amount + ']' ]
-    })   
-  }
+  const deposit = () => {
+    // this.setState({
+    //   balance: Number(this.state.balance) + Number(this.state.amount),
+    //   transactionHistory: [...this.state.transactionHistory, 'Deposit (' + this.state.amount + ')' ]
+    // })   
+    return setBalance(Number(balance) + Number(amount)),
+           setTransactionHistory([...transactionHistory, 'Deposit ( ' + amount + ' )' ])
 
-  deposit () {
-    this.setState({
-      balance: Number(this.state.balance) + Number(this.state.amount),
-      transactionHistory: [...this.state.transactionHistory, 'Deposit [' + this.state.amount + ']' ]
-    })   
   }  
 
-  showBalance () {
-    return this.state.balance
+  const showBalance = () => {
+    // return this.state.balance
+    return balance
   }
   
-  handleChange (event) {
-    this.setState({
-      amount: event.target.value
-    })
+  const handleChange = (event) => {
+    // this.setState({
+    //   amount: event.target.value
+    // })
+    setAmount(event.target.value)
   }
     
-  render () {
+  // render () {
     return (
       <div>
-        <p>Bank Account Type: {this.getType()}</p>
+        {/* <p>Bank Account Type: {this.getType()}</p> */}
+        <p>Bank Account Type: {getType()}</p>
         <input 
           type="text"
-          onChange={this.handleChange}
-          value={this.state.amount}
+          // onChange={this.handleChange}
+          // value={this.state.amount}
+          onChange={handleChange}
+          value={amount}
         />
-        <button onClick={this.withdraw}>Withdraw</button>
-        <button onClick={this.deposit}>Deposit</button>
-        <p>Current Balance: {this.showBalance()}</p>
-        <p>Transaction History: {this.state.transactionHistory.join(', ')}</p>
+        {/* <button onClick={this.withdraw}>Withdraw</button> */}
+        <button onClick={withdraw}>Withdraw</button>
+        {/* <button onClick={this.deposit}>Deposit</button> */}
+        <button onClick={deposit}>Deposit</button>
+        {/* <p>Current Balance: {this.showBalance()}</p> */}
+        <p>Current Balance: {showBalance()}</p>
+        {/* <p>Transaction History: {this.state.transactionHistory.join(', ')}</p> */}
+        <p>Transaction History: {transactionHistory.join(', ')}</p>
       </div>
     )
-  }
+  // }
 }
